47b357f556634279f9bab5f9b7f0db9b4c092f6c
MGentieu <mgentieu02@gmail.com>
2024-12-22 13:51:38 +0000

    Commentaires pour le main

--- main/main.py
+++ main/main.py
@@ -14,19 +14,23 @@ def execute_command(command):
         print(f"Une erreur s'est produite : {e}")
 
 
-# 3. Traitement des Données avec Spark
+# On transforme à chaque fois l'attribut 'date', que l'on décompose en année, mois, jour :
 execute_command("spark-submit --deploy-mode client --master local[2] TransfoDate.py")
 
+# On gère aussi les valeurs manquantes comme les températures pour chaque fichier CSV :
+# On change aussi la latitude et la longitude pour donner une valeur réelle, positive si N ou E, négative si O ou S :
 execute_command("spark-submit --deploy-mode client --master local[2] fill_missing_GLTBC.py") 
 execute_command("spark-submit --deploy-mode client --master local[2] fill_missing_GLTBMC.py")
 execute_command("spark-submit --deploy-mode client --master local[2] fill_missing_GT.py")
 execute_command("spark-submit --deploy-mode client --master local[2] fill_missing_GLTBS.py") 
 execute_command("spark-submit --deploy-mode client --master local[2] fill_missing_GLTBCi2.py") 
 
+# On exécute ces commandes pour déplacer, renommer, et supprimer des fichiers afin d'obtenir à la fin les fichiers traités
+# avec les mêmes noms que les fichiers orignaux, cela afin de faciliter le traitement à postériori
 execute_command("chmod +x commandes_suppr_fichiers_inutiles.sh")
 execute_command("./commandes_suppr_fichiers_inutiles.sh")
 
-#Visualisation graphs
+# Les données normalisées et nettoyées, on exécute à présent des fichiers afin d'afficher des graphiques et ainsi analyser les tendances :s
 
 
 
